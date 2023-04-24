f708680582fdd2f7f0118f414ae0cf59349721f7
kaank45 <130989267+kaank45@users.noreply.github.com>
2023-04-24 18:33:33 +0300

    Delete src directory

--- src/main/java/com/example/recipeapi/strategy/ListingStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.example.recipeapi.strategy;
-import java.util.List;
-
-import com.example.recipeapi.repository.RecipeRepository;
-
-public interface ListingStrategy {
-    List<String> list(RecipeRepository recipeRepository);
-
-}
